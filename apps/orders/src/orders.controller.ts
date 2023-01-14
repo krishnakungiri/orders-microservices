7cbab67f36ae194c8383810438c0e971524a6e4a
Krishna navtech <krishna.k@navtech.io>
2023-01-14 10:13:54 +0530

    a

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
+import { Body, Post } from '@nestjs/common/decorators';
+import { createOrderRequest } from './dto/create-order.requests';
 import { OrdersService } from './orders.service';
 
-@Controller()
+@Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @Post()
+  async createOrder(@Body() request: createOrderRequest){
+    return this.ordersService.createOrder(request);
+  }
+
   @Get()
-  getHello(): string {
-    return this.ordersService.getHello();
+  async getOrders(){
+    return this.ordersService.getOrders();
   }
 }
--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,8 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { BILLING_SERVICE } from './constants/services';
+import { createOrderRequest } from './dto/create-order.requests';
+import { OrdersRepository } from './orders.repository';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly ordersRepository: OrdersRepository,
+    @Inject(BILLING_SERVICE) private billingClient: ClientProxy
+  ) { }
+
+  async createOrder(request: createOrderRequest){
+  //  return this.ordersRepository.create(request)
+
+    // const session = await this.ordersRepository.startTransaction()
+    try {
+      const order = await this.ordersRepository.create(request)
+      await lastValueFrom(
+        this.billingClient.emit('order_created', {
+          request
+        })
+      )
+
+      // await session.commitTransaction()
+      return order
+
+    } catch (error) {
+      // await session.abortTransaction()
+      throw error;
+    }
+  }
+
+  async getOrders(){
+    return this.ordersRepository.find({})
   }
 }
